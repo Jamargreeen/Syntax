b3cb55aa6b62c10c14f9c32edb17dc56db379887
Jamar Green <jamargreen22@gmail.com>
2021-12-12 00:09:55 -0600

    initial commit

---
+++ Syntax/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Syntax
+{
+    class Program
+    {
+        public static void Main(string[] args,
+            //int answer = 4;
+            //string response;
+            //if (answer< 9)
+            //{
+            //      response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //      response = answer + “greater than or equal to nine”;
+            //}
+
+        var answer = 4;
+        var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine or equal the number nine";
+
+    }
+}
